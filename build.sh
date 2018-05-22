9c744400028f69ba41965b429baa96016bd10bef
RJ Bergeron <rbergeron@palantir.com>
2018-05-22 17:49:53 -0400

    handle image tagging like this

--- build.sh
+++ build.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+set -ex
+
+[ "${DOCKER_SINK}" ] && {
+  docker pull "${DOCKER_SINK}/ubuntu:xenial"
+  docker tag  "${DOCKER_SINK}/ubuntu:xenial" "ubuntu:xenial"
+}
+
+set -u
+
 for d in dockerfiles/*/Dockerfile ; do
   image=${d#dockerfiles/}
   image=${image%/Dockerfile}
