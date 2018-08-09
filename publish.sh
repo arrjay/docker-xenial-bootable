2f212dc47b56afb0d4cee6c46ee605f645e0d657
RJ Bergeron <rbergeron@palantir.com>
2018-08-09 15:35:59 -0700

    make a bionic version. will sed be enough?

--- publish.sh
+++ publish.sh
@@ -8,11 +8,11 @@ for img in $(docker images "build/*" --format "{{.Repository}}:{{.Tag}}") ; do
   dest="${img#build/}"
   dest="${dest%:latest}"
   case "${dest}" in [0-9]*) continue ;; esac
-  docker tag "${img}" "${DOCKER_SINK}/ubuntu-16.04-vm-bootable:${dest}"
-  docker tag "${img}" "${DOCKER_SINK}/ubuntu-16.04-vm-bootable:${dest}.${ts}"
+  docker tag "${img}" "${DOCKER_SINK}/ubuntu-18.04-vm-bootable:${dest}"
+  docker tag "${img}" "${DOCKER_SINK}/ubuntu-18.04-vm-bootable:${dest}.${ts}"
   [ "${NOPUSH}" ] || {
-    docker push "${DOCKER_SINK}/ubuntu-16.04-vm-bootable:${dest}"
-    docker push "${DOCKER_SINK}/ubuntu-16.04-vm-bootable:${dest}.${ts}"
+    docker push "${DOCKER_SINK}/ubuntu-18.04-vm-bootable:${dest}"
+    docker push "${DOCKER_SINK}/ubuntu-18.04-vm-bootable:${dest}.${ts}"
   }
 done
 
