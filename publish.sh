ada9b292d25b39a423a22cab1d2927ca86a2316d
RJ Bergeron <rbergero@gmail.com>
2018-08-20 02:06:08 -0400

    switch all of this to minifed package installs as well

--- publish.sh
+++ publish.sh
@@ -5,12 +5,13 @@ ts=$(date +%s)
 for img in $(docker images "build/*" --format "{{.Repository}}:{{.Tag}}") ; do
   dest="${img#build/}"
   dest="${dest%:latest}"
-  case "${dest}" in [0-9]*) continue ;; esac
+  case "${dest}" in [0-9]*) docker rmi "${img}" ; continue ;; esac
   docker tag "${img}" "${DOCKER_SINK}/xenial-bootable:${dest}"
   docker tag "${img}" "${DOCKER_SINK}/xenial-bootable:${dest}.${ts}"
   [ "${NOPUSH}" ] || {
     docker push "${DOCKER_SINK}/xenial-bootable:${dest}"
     docker push "${DOCKER_SINK}/xenial-bootable:${dest}.${ts}"
   }
+  docker rmi "${img}"
 done
 
